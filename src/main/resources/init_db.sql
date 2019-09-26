d50b7a25f7e26b0459ce17e5453a15a39588ad3d
Тарас <tarashryts@gmail.com>
2019-09-26 11:09:21 +0300

    add init_db.sql correcting

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -5,18 +5,3 @@ CREATE TABLE `newdatabase`.`items` (
   `name` VARCHAR(225) NOT NULL,
   `price` DECIMAL(6,4) NOT NULL,
   PRIMARY KEY (`item_id`));
-
-ALTER TABLE `newdatabase`.`items`
-CHANGE COLUMN `price` `price` DECIMAL(10,4) NOT NULL ;
-
-INSERT INTO `newdatabase`.`items` (`name`, `price`) VALUES ('mersedes', '10');
-
-SELECT * FROM newdatabase.items where item_id=1;
-
-SELECT * FROM newdatabase.items;
-
-DELETE FROM newdatabase.items WHERE item_id=2;
-
-UPDATE newdatabase.items SET item_id=300 WHERE item_id=3;
-
-UPDATE newdatabase.items SET item_id=302, name='newcar', price=999.1 WHERE item_id=4;
